e8ff13c2397c871a12fbf2827789930e518a37e9
Santiago <santiago.torress@upb.edu.co>
2022-02-09 10:51:54 -0500

    Identificamos las variables

--- problem/program.asm
+++ problem/program.asm
@@ -2,5 +2,18 @@
 // 000431583
 // santiago.torress@upb.edu.co
 (START)
-    @START
+//Identificar las variables//
+    @70
+    D=A
+    @codeF
+    M=D
+    @67
+    D=A
+    @codeC
+    M=D
+    @24576 //Lector al presionar una tecla//
+    D=A
+    @tecla
+    M=D
+
     0;JMP
