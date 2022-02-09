2e970129f11339f3e8fc811538b06fe451354aa5
Santiago <santiago.torress@upb.edu.co>
2022-02-09 10:21:51 -0500

    Información solicitada

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Santiago Torres Saldarriaga
+// 000431583
+// santiago.torress@upb.edu.co
 (START)
     @START
     0;JMP
